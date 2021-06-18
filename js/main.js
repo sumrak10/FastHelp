5e05633bbed6bd16beef9bb777b98ae0421201ef
sumrak10 <nurlan.urazkulov@gmail.com>
2021-06-19 00:08:40 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let main_button = document.getElementsByClassName('main-button')[0]
+    main_button.addEventListener('click', (e) => {
+        main_button.classList.add('')
+    })
+});
